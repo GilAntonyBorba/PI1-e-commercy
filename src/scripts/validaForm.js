49caaf5460dc0f1afeffb5a5030707e952b2ec4b
GilAntonyBorba <gilantonybvao@gmail.com>
2023-11-06 22:06:52 -0300

    Voltando a ter apenas a validação do formulário feita, e agora os comentários foram retirados

--- src/scripts/validaForm.js
+++ src/scripts/validaForm.js
@@ -1,77 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function resetForm() {
-        // Limpa os campos
-        document.forms.SignUp.InputName.value = '';
-        document.forms.SignUp.InputEmail.value = '';
-        document.forms.SignUp.InputPassword.value = '';
-        document.querySelector('input[name="conheceu"]').checked = false;
-        document.querySelector('input[name="conheciaAntes"]').checked = false;
-
-        // Habilita os campos para edição
-        document.forms.SignUp.InputName.disabled = false;
-        document.forms.SignUp.InputEmail.disabled = false;
-        document.forms.SignUp.InputPassword.disabled = false;
-        document.querySelector('input[name="conheceu"]').disabled = false;
-        document.querySelector('input[name="conheciaAntes"]').disabled = false;
-
-        // Fecha o modal
-        $('#meuModal').modal('hide');
-    }
-
-    document.getElementById('btnAlterar').addEventListener('click', function() {
-        // Habilita os campos para edição
-        document.forms.SignUp.InputName.disabled = false;
-        document.forms.SignUp.InputEmail.disabled = false;
-        document.forms.SignUp.InputPassword.disabled = false;
-        document.querySelector('input[name="conheceu"]').disabled = false;
-        document.querySelector('input[name="conheciaAntes"]').disabled = false;
-
-        resetForm();
-    });
-
-    // Botão "Limpar dados"
-    document.getElementById('btnLimpar').addEventListener('click', resetForm);
-
-    // Atribuir a função validaForm ao evento onsubmit do formulário
+document.addEventListener('DOMContentLoaded', () => {
     document.forms.SignUp.onsubmit = validaForm;
-
-    // Botão "Enviar"
-    document.getElementById('btnEnviarFormulario').addEventListener('click', function(e) {
-        if (!formValido) {
-            // Mostra a modal apenas se !formValido
-            $('#meuModal').modal('show');
-        } else {
-            // Trate o caso em que o formulário seja válido (adicione aqui o código para o envio do formulário, se necessário)
-        }
-    });
-
-     // Evento que abre a janela modal
-     $('#meuModal').on('shown.bs.modal', function () {
-        $('#meuInput').trigger('focus');
-        displayFormData();
-    });
-
-    function displayFormData() {
-        const name = document.forms.SignUp.InputName.value;
-        const email = document.forms.SignUp.InputEmail.value;
-        const password = document.forms.SignUp.InputPassword.value;
-        const conheceu = document.querySelector('input[name="conheceu"]:checked');
-        const conheciaAntes = document.querySelector('input[name="conheciaAntes"]:checked');
-
-        const modalBody = document.querySelector('.modal-body');
-        modalBody.innerHTML = `
-            <p><strong>Nome:</strong> ${name}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Senha:</strong> ${password}</p>
-            <p><strong>Por onde nos conheceu:</strong> ${conheceu ? conheceu.nextSibling.textContent : 'Não selecionado'}</p>
-            <p><strong>Já nos conhecia anteriormente:</strong> ${conheciaAntes ? conheciaAntes.nextSibling.textContent : 'Não selecionado'}</p>
-        `;
-    }
-
 });
 
 function validaForm(e){
-    e.preventDefault();
     let form = e.target;//target é o formulário que gerou essa submissão
     let formValido = true;
     console.log("início da função validaForm!");
@@ -101,6 +32,7 @@ function validaForm(e){
     else{
         form.InputName.classList.remove("inputDestacado");
     }
+
 
     // EMAIL-=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=-=-=-=-=--=-=
         if(form.InputEmail.value === ""){
@@ -181,9 +113,9 @@ function validaForm(e){
         spanRadiobox.style.display = 'none'; // Oculta a mensagem de erro
         pRadio.classList.remove("pDestacado");
     }
+
     
     if (!formValido){
-        
-        
+        e.preventDefault();
     }
 }
